cf2c599e916818ad612171a0c0b39b10c1d4745d
farran <farranwang@gmail.com>
2018-04-26 23:07:27 -0400

    some message

--- HW_5_14.py
+++ HW_5_14.py
@@ -2,6 +2,7 @@
 
 def mergeSort(alist, start, end):
 	print("Splitting ",alist)
+	newlist = list()
 	if end > start:
 		mid = (start + end)//2
 		mergeSort(alist, start, mid)
@@ -11,23 +12,26 @@ def mergeSort(alist, start, end):
 		j=mid+1
 		k=start
 		while i < mid+1 and j < end+1:
-			if alist[i] < alist[j]:
-				alist[k]=alist[i]
+			if newlist[i] < newlist[j]:
+				newlist[k]=newlist[i]
 				i=i+1
 			else:
-				alist[k]=alist[j]
+				newlist[k]=newlist[j]
 				j=j+1
 			k=k+1
 
 		while i < mid+1:
-			alist[k]=alist[i]
+			newlist[k]=newlist[i]
 			i=i+1
 			k=k+1
 
 		while j < end+1:
-			alist[k]=alist[j]
+			newlist[k]=newlist[j]
 			j=j+1
 			k=k+1
+
+	for o in range(0, len(newlist)-1):
+		alist[o] = newlist[o]
 
 	print("Merging ",alist)
 
